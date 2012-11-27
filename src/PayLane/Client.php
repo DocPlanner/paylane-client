d2e820fbf1e57633799521843dc9fe91cf6f6dbe
Maciej Łebkowski <m.lebkowski@gmail.com>
2012-11-27 17:22:25 +0100

    Remove the legacy api, since it was shit

--- src/PayLane/Client.php
+++ src/PayLane/Client.php
@@ -4,22 +4,14 @@ namespace PayLane;
 
 use \SoapClient, \SoapFault;
 
-require_once 'PayLaneClient.php';
-
-class Client extends \PayLaneClient
+class Client
 {
 	const WSDL = 'https://direct.paylane.com/wsdl/production/Direct.wsdl';
 
 	/**
 	 * @var SoapClient
 	 */
-	protected $soapclient;
-
-	/**
-	 * @var SoapFault
-	 */
-	protected $fault;
-	protected $error;
+	protected $soapClient;
 
 
 	/**
@@ -38,30 +30,90 @@ class Client extends \PayLaneClient
 			$wsdl = self::WSDL;
 		}
 
-		$this->soapclient = new SoapClient($wsdl, array("login" => $login, "password" => $password));
+		$this->soapClient = new SoapClient($wsdl, array("login" => $login, "password" => $password));
 	}
 
 	/**
-	 * @deprecated
+	 * Performs sale
 	 *
-	 * @return bool
+	 * @param array $params
+	 * @return Object
+	 * @throws SoapFault
 	 */
-	public function init()
+	public function multiSale($params)
 	{
-		return true;
-	}
-	protected function handleFault($fault)
-	{
-		$this->fault = $fault;
-		$this->error = "SOAPFault: " . $fault->faultcode . " - " . $fault->faultstring;
+		return $this->soapClient->multiSale($params);
 	}
 
 	/**
-	 * @return \SoapFault
+	 * Performs refund
+	 *
+	 * @param array $params
+	 * @return Object
+	 * @throws SoapFault
 	 */
-	public function getLastFault()
+	public function refund($params)
 	{
-		return $this->fault;
+		return $this->soapClient->refund($params['id_sale'], $params['amount'], $params['reason']);
 	}
 
+	/**
+	 * Performs resale
+	 *
+	 * @param array $params
+	 * @return Object
+	 * @throws SoapFault
+	 */
+	public function resale($params)
+	{
+		return $this->soapClient->resale($params['id_sale'], $params['amount'], $params['currency'], $params['description']);
+	}
+
+	/**
+	 * Performs sale capture
+	 *
+	 * @param array $params
+	 * @return Object
+	 * @throws SoapFault
+	 */
+	public function captureSale($params)
+	{
+		return $this->soapClient->captureSale($params['id_sale_authorization'], $params['amount'], $params['description']);
+	}
+
+	/**
+	 * Performs sale authorization closing
+	 *
+	 * @param array $params
+	 * @return Object
+	 * @throws SoapFault
+	 */
+	public function closeSaleAuthorization($params)
+	{
+		return $this->soapClient->closeSaleAuthorization($params['id_sale_authorization']);
+	}
+
+	/**
+	 * Checks if card participates in 3D Secure
+	 *
+	 * @param array $params
+	 * @return Object
+	 * @throws SoapFault
+	 */
+	public function checkCard3DSecureEnrollment($params)
+	{
+		return $this->soapClient->checkCard3DSecureEnrollment($params);
+	}
+
+	/**
+	 * Checks status of multiple sales
+	 *
+	 * @param array $params
+	 * @return Object
+	 * @throws SoapFault
+	 */
+	public function checkSales($params)
+	{
+		return $this->soapClient->checkSales(array("id_sale_list" => $params));
+	}
 }
